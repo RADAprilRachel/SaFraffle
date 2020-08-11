cdf72155ddeffba8c315e942e3c98ce4fe7ae4f0
Rachel Determann <RAD.April.Rachel@gmail.com>
2020-08-11 19:25:11 +0000

    Move tests to appropriate loaction

--- tests/Feature/UserAthenticationTest.php
+++ tests/Feature/UserAthenticationTest.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
 use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class UserTest extends TestCase
+class UserAthenticationTest extends TestCase
 {
     private function pageAuth($role, $page, $status)
     {
